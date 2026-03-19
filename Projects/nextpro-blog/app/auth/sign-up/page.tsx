7bcbb05eab589753aee62a57103292fba66040e5
Sami Munir <samidmunir@outlook.com>
2026-03-19 05:55:08 -0400

    implementing shared layout & creating auth/signup route

---
+++ Projects/nextpro-blog/app/auth/sign-up/page.tsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function SignUpPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign up</CardTitle>
+        <CardDescription>Create an account to get started</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form></form>
+      </CardContent>
+    </Card>
+  );
+}
